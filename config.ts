0ddef51b9b3b3d0128fb65a2ab47a7ec8fcaa7dd
qwfy <qwfy5287@qq.com>
2024-03-06 11:12:08 +0800

    feat: rust notify 6.1.1 docs

--- config.ts
+++ config.ts
@@ -1,8 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  // url: "https://www.builder.io/c/docs/developers",
+  // match: "https://www.builder.io/c/docs/**",
+  url: "https://docs.rs/notify/latest/notify/all.html",
+  match: "https://docs.rs/notify/latest/notify/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
